ddf40c3ef76a79a798b9bdfbbc3e94ff71e5daf2
Mark Dobbins <mjdobbins12@gmail.com>
2020-01-29 15:40:32 +0000

    test passed for adding single item

--- prices.js
+++ prices.js
@@ -7,4 +7,4 @@ const priceList = {
 	"Apples": 100
 };
 
-modules.exports = priceList;
+module.exports = priceList;
--- spec/basketPricerSpec.js
+++ spec/basketPricerSpec.js
@@ -1,12 +1,18 @@
 const BasketPricer = require('../lib/basketPricer');
+const priceList = require('../prices');
 
 describe('BasketPricer', function() {
 	beforeEach(function() {
-		bp = new BasketPricer();	
+		bp = new BasketPricer();
 	});
 	
 	it('has an inital subtotal and total amount of 0', function() {
 		expect(bp.subtotal).toEqual(0);
 		expect(bp.total).toEqual(0);	
-	});	
+	});
+	
+	it('increments the subtotal when a single item is added', function() {
+		bp.addItems(["Apples"], priceList);
+		expect(bp.subtotal).toEqual(priceList["Apples"]);	
+	});
 });
